aed134ef206b6eb7b3f4d8a697358caafcfb11db
tomit4 <mosssap@gmail.com>
2024-06-06 17:34:32 -0700

    :pencil2: Conform to arrow style syntax

--- backend/src/lib/plugins/index.ts
+++ backend/src/lib/plugins/index.ts
@@ -2,12 +2,12 @@ import type { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import type { WebSocket } from '@fastify/websocket'
 export default async (fastify: FastifyInstance): Promise<void> => {
     await fastify.register(import('@fastify/websocket'), {
-        errorHandler: function (
+        errorHandler: (
             _: Error,
             socket: WebSocket,
             __: FastifyRequest,
             ___: FastifyReply,
-        ) {
+        ) => {
             socket.terminate()
         },
         options: {
