011a5261dd1dc96124e0177f1970099f5ea356bc
tomit4 <mosssap@gmail.com>
2024-06-12 16:56:25 -0700

    :construction: Imported pong tutorial in prep for game feature

--- frontend/src/public/js/index.js
+++ frontend/src/public/js/index.js
@@ -73,3 +73,68 @@ form.addEventListener('submit', event => {
     socket.send(message.value);
     form.reset();
 });
+let board = document.getElementById('board');
+const boardWidth = 500;
+const boardHeight = 500;
+let context = board.getContext('2d');
+const playerWidth = 10;
+const playerHeight = 50;
+const playerVelocityY = 0;
+const player1 = {
+    x: 10,
+    y: boardHeight / 2,
+    width: playerWidth,
+    height: playerHeight,
+    velocityY: playerVelocityY,
+};
+const player2 = {
+    x: boardWidth - playerWidth - 10,
+    y: boardHeight / 2,
+    width: playerWidth,
+    height: playerHeight,
+    velocityY: playerVelocityY,
+};
+window.onload = () => {
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context.fillStyle = 'skyblue';
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+    requestAnimationFrame(update);
+    document.addEventListener('keyup', movePlayer);
+};
+const update = () => {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+    context.fillStyle = 'skyblue';
+    let nextPlayer1Y = player1.y + player1.velocityY;
+    if (!outOfBounds(nextPlayer1Y))
+        player1.y = nextPlayer1Y;
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+    context.fillStyle = 'skyblue';
+    let nextPlayer2Y = player2.y + player2.velocityY;
+    if (!outOfBounds(nextPlayer2Y))
+        player2.y = nextPlayer2Y;
+    context.fillRect(player2.x, player2.y, player2.width, player2.height);
+    for (let i = 10; i < board.height; i += 25) {
+        context.fillRect(board.width / 2 - 10, i, 5, 5);
+    }
+};
+const outOfBounds = (yPosition) => {
+    return yPosition < 0 || yPosition + playerHeight > boardHeight;
+};
+const movePlayer = (e) => {
+    if (e.code === 'KeyW')
+        player1.velocityY = -3;
+    else if (e.code === 'KeyS')
+        player1.velocityY = 3;
+    if (e.code === 'ArrowUp')
+        player2.velocityY = -3;
+    else if (e.code === 'ArrowDown')
+        player2.velocityY = 3;
+};
+const detectCollision = (a, b) => {
+    return (a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y);
+};
